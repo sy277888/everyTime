a8db89f441877c8794d8b83585f7ab21b5e425fb
sy277888 <73463011+sy277888@users.noreply.github.com>
2020-10-28 14:25:16 +0800

    登录修改

--- src/components/geren/geren.vue
+++ src/components/geren/geren.vue
@@ -1,24 +1,62 @@
 <template>
   <div>
-      <van-nav-bar title="个人信息" left-arrow @click-left="onClickLeft" />
+    <van-nav-bar title="个人信息" left-arrow @click-left="onClickLeft" />
+    <div class="sy_geren">
+      <ul>
+        <li>
+          <span>头像</span>
+          <p><span>></span></p>
+        </li>
+        <li>   <span>昵称</span>
+          <p><span>></span></p></li>
+        <li>   <span>手机号</span>
+          <p><span>></span></p></li>
+        <li>   <span>性别</span>
+          <p><span>></span></p></li>
+        <li>   <span>出生日期</span>
+          <p><span>></span></p></li>
+        <li>   <span>所在城市</span>
+          <p><span>></span></p></li>
+          <li>   <span>学科</span>
+          <p><span>></span></p></li>
+          <li>   <span>年级</span>
+          <p><span>></span></p></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-data() {
-    return {
-        
-    }
-},
-methods: {
-    onClickLeft(){
-        this.$router.push({path:"/wode"})
-    }
-},
-}
+  data() {
+    return {};
+  },
+  methods: {
+    onClickLeft() {
+      this.$router.push({ path: "/mime" });
+    },
+  },
+  mounted(){
+      this.$Net.xue().then(res=>{
+          console.log(res);
+      })
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.sy_geren {
+    width: 100%;
+    margin-top: 2.4rem;
 
+}
+.sy_geren ul li {
+    width: 95%;
+    height: 2rem;
+    margin-left:1rem;
+    display: inline-flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top:1rem ;
+}
 </style>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -52,10 +52,10 @@ export default {
         .then((res) => {
           console.log(res);
           // localStorage.token = res.data.token;
-          var token=res.data.token;
+          var token=res.data.data.remember_token;
           localStorage.setItem("token",token);
           localStorage.setItem("username",this.username)
-          this.$router.push({ path: "/wode" });
+          this.$router.push({ path: "/mime" });
         })
         .catch((err) => {
           console.log(err);
