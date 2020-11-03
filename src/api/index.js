48008d144a3ac9bac04ee02ab69c8276daa79388
黄孟雯 <email@1257731795.com>
2020-11-03 13:07:30 +0800

    收藏完美解决

--- src/api/index.js
+++ src/api/index.js
@@ -20,15 +20,15 @@ const Net = {
   },
   // 获取详情页面数据
   courseXQList(params) {
-    return request(API.Method.Get, API.Path.HMWXQ, params)
+    return request(API.Method.Get, API.Path.HMWXQ+params)
   },
   // 设置收藏
   courseXQSC(params) {
-    return request(API.Method.Post, API.Path.HMWSC, params)
+    return request(API.Method.Post, API.Path.HMWSC,params)
   },
   // 取消收藏
   courseXQSCNO(params) {
-    return request(API.Method.Post, API.Path.HMWSCNO, params)
+    return request(API.Method.Put,'/api/app/collect/cancel/'+params+'/1')
   },
   // 课程评价数据
   courseXQPJ(params) {
--- src/main.js
+++ src/main.js
@@ -33,6 +33,7 @@ Vue.filter("timefn",function(originval){
 
   return `${mm}月${dd}日 ${hh}:${ff}`
 })
+// 详情页
 Vue.filter("timefnxq",function(originval){
   const dt = new Date(originval)
 
@@ -44,6 +45,19 @@ Vue.filter("timefnxq",function(originval){
   const ss = (dt.getSeconds() +'' ).padStart(2,0)
 
   return `${yy}.${mm}.${dd} ${hh}:${ff}`
+})
+// 详情课程评论
+Vue.filter("timeEvaluate",function(originval){
+  const dt = new Date(originval)
+
+  const yy = dt.getFullYear()
+  const mm = (dt.getMonth() +1+'' ).padStart(2,0)
+  const dd = (dt.getDate() +'' ).padStart(2,0)
+  const hh = (dt.getHours() +'' ).padStart(2,0)
+  const ff = (dt.getMinutes() +'' ).padStart(2,0)
+  const ss = (dt.getSeconds() +'' ).padStart(2,0)
+
+  return `${yy}-${mm}-${dd} ${hh}:${ff}`
 })
 // 修复全局点击重复路由bug
 import Router from 'vue-router'
